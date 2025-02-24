3f085911641f2afa1ea11042d6d8ace553a56bfa
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2025-02-24 14:23:47 +0300

    Update DenemeTest.php

--- myapphp/tests/DenemeTest.php
+++ myapphp/tests/DenemeTest.php
@@ -10,6 +10,8 @@ class DenemeTest extends TestCase
          $this->assertTrue(false, "test".__DIR__."Constructor private olmalı");
         // Reflection kullanarak private constructor'ı açığa çıkar
         $reflection = new ReflectionClass('deneme');
+        var_export($reflection);
+        $this->assertTrue(false, $reflection);
         $constructor = $reflection->getConstructor();
         
         // Constructor'ın private olup olmadığını test et
